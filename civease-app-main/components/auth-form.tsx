bc167b05708b67aecb86427cec4547d8241db57f
ABHIJEET <abhijeetdas1423@gmail.com>
2025-11-02 11:18:30 +0530

    Changes

---
+++ civease-app-main/components/auth-form.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AuthForm = () => {
+  return (
+    <form>
+      {/* Add your form fields here */}
+    </form>
+  );
+};
+
+export default AuthForm;
+{
+  "name": "your-project-name",
+  "version": "1.0.0",
+  "scripts": {
+    "dev": "node index.js"
+  },
+  "dependencies": {
+    // ...your dependencies...
+  }
+}
